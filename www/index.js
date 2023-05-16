e4546474fee4e2f9359414b605b322a9be7af99e
owowed <island@owowed.moe>
2023-05-16 17:41:24 +0700

    randomized name script cleanup
    Signed-off-by: owowed <island@owowed.moe>

--- www/index.js
+++ www/index.js
@@ -4,19 +4,32 @@ const bodyColors = await fetch("./data/body-colors.json").then(i => i.json());
 
 const azkaMessage = document.querySelector("#azka-message");
 
-const helloNamesIteration = iterateCycle(helloNames);
-const bodyColorsIteration = iterateCycle(bodyColors);
-
-setInterval(() => {
-    azkaMessage.textContent = `Hello, ${helloNamesIteration.next().value}!`;
-    document.body.style.backgroundColor = bodyColorsIteration.next().value;
-}, 3000);
-
-function* iterateCycle(array) {
+const iterateCycle = function* (array) {
     let ind = 0;
     while (true) {
         if (ind == array.length) ind = 0;
 
         yield array[ind++];
     }
-}
+};
+
+const bodyColorsIteration = iterateCycle(bodyColors);
+
+let lastHelloNameIndex = 0;
+
+const intervalFunction = () => {
+    let helloNameIndex = Math.floor(Math.random() * helloNames.length);
+    
+    while (helloNameIndex == lastHelloNameIndex) {
+        helloNameIndex = Math.floor(Math.random() * helloNames.length);
+    }
+
+    azkaMessage.textContent = `Hello, ${helloNamesIteration[helloNameIndex]}!`;
+    document.body.style.backgroundColor = bodyColorsIteration.next().value;
+
+    lastHelloNameIndex = helloNameIndex;
+};
+
+intervalFunction();
+
+setInterval(intervalFunction, 3000);
